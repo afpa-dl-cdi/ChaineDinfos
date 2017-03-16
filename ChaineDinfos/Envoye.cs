1c46d022784631b7f6159d52f9b47aef1baf6403
bdorier <benjamin.dorier@hotmail.fr>
2017-03-16 15:22:25 +0100

    recup master

--- ChaineDinfos/Envoye.cs
+++ ChaineDinfos/Envoye.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChaineDinfos
 {
-    sealed class Envoye : Journaliste
+    class Envoye : Journaliste
     {
         /// <summary>
         /// Zone d'affectation
@@ -18,15 +18,25 @@ namespace ChaineDinfos
         /// </summary>
         public Poste PosteDeLEnvoye { get; set; }
 
-
+        /// <summary>
+        /// Filme un reportage
+        /// </summary>
+        /// <returns>Reportage</returns>
         public Reportage Filmer()
         {
-            Console.WriteLine("Je filme");
+            Console.WriteLine("Je filme pour {0}", chaineTV );
             Reportage reportage = new Reportage();
             reportage.sujetDuReportage = "Final de pétanque 2017";
             return reportage;
         }
 
+        /// <summary>
+        /// Rédige un reportage
+        /// </summary>
+        /// <param name="texteduSujet"></param>
+        /// <param name="dateDuSujet"></param>
+        /// <param name="titreDuSujet"></param>
+        /// <returns>Reportage</returns>
         public override Reportage Rediger(string texteduSujet, DateTime dateDuSujet, string titreDuSujet)
         {
             Console.WriteLine("je rédige un reportage");
@@ -35,7 +45,7 @@ namespace ChaineDinfos
             return reportage;
         }
 
-         
+
 
     }
 }
